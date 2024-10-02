12e2b49782c6a14a9a1b669f4fd91dc1875253a4
CAGULA770 <166716920+CAGULA770@users.noreply.github.com>
2024-10-02 21:02:55 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = 393533870586'  //creator number
+global.ownername = 'Cagula org👑' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
